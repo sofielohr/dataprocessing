372c87a2ec1dac08421abc178088f71d3c1fb809
Sofie Löhr <sofielohr@MacBook-Pro-van-Sofie-2.local>
2019-03-06 22:05:45 +0100

    LATE SUBMISSION week 4 nog niet af

---
+++ Homework/Week_4/convert.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# Name: Sofie Löhr
+# Student number: 11038926
+"""
+This script parses and processes the data from a csv file
+"""
+
+import csv
+import pandas as pd 
+import numpy
+import json
+
+# Global constants
+INPUT = "data.csv"
+OUTPUT_FILE = "data.json"
+YEAR = 1980
+MEASURE = "KTOE"
+
+def processing(file):
+	"""
+	Function cleaning and processing file	
+	Keep for every country the MEASURE value in the YEAR, both specified in the global constant
+	Delete the missing values
+	Delete OECD Total; gives a distored image of the values since OECD is the sum of all the other countries values
+	"""
+
+	# Delete INDICATOR, SUBJECT, FREQUENCY and Flag Code, not necessary
+	file = file.drop(['INDICATOR', 'SUBJECT', 'FREQUENCY', 'Flag Codes'], axis=1)
+
+	# Get only MEASURE
+	file = file.loc[file['MEASURE'] == MEASURE]
+	file = file.loc[file['TIME'] == YEAR]
+
+	# Delete the missing values
+	file = file.dropna()
+
+	# Delete OECD value
+	file = file.loc[file['LOCATION'] != 'OECD']
+
+	# Sort values
+	file = file.sort_values(by='Value', ascending=False)
+
+	print(file)
+	
+	return(file)
+
+if __name__ == "__main__":
+
+	# open input file
+	with open(INPUT) as in_file:
+		data_frame = pd.read_csv(in_file, skip_blank_lines=True)
+	
+	# process data
+	data_frame = processing(data_frame)
+
+	# export to json
+	export = data_frame.to_json(OUTPUT_FILE, orient='records')
