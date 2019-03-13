bec02285b29ff472e719ad75bdb7dbfaf200cca9
Sofie Löhr <sofielohr@MacBook-Pro-van-Sofie-2.local>
2019-03-13 21:24:08 +0100

    final version week 5

--- Homework/Week_5/scatter.js
+++ Homework/Week_5/scatter.js
@@ -1,15 +1,10 @@
 // Sofie Löhr, 11038926
 
 // loading data
-var tourismInbound = "https://stats.oecd.org/SDMX-JSON/data/TOURISM_INBOUND/AUS+AUT+BEL+BEL-BRU+BEL-VLG+BEL-WAL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA+NMEC+ARG+BRA+BGR+CHN+COL+CRI+HRV+EGY+MKD+IND+IDN+MLT+MAR+PER+PHL+ROU+RUS+ZAF.INB_ARRIVALS_TOTAL/all?startTime=2009&endTime=2017"
-var purchasingPowerParities = "https://stats.oecd.org/SDMX-JSON/data/PPPGDP/PPP.AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA+EA18+OECD/all?startTime=2009&endTime=2017&dimensionAtObservation=allDimensions"
-
 var requests = [d3.json("gdp.json"), d3.json("ppp.json"), d3.json("tourists.json")];
 
 Promise.all(requests).then(function(response) {
     plotFun(response);
-}).catch(function(e){
-    throw(e);
 });
 
 // initialize clean data
@@ -20,8 +15,8 @@ let clean_data = {
 	"2015": [],
 	"2016": [],
 	"2017": [],
-	"2018": [] 
-}
+	"2018": []
+};
 
 function plotFun(input) {
 	// process data
@@ -31,42 +26,81 @@ function plotFun(input) {
 		// iterate over years
 		d.forEach(function(e) {
 			// make object for country
-			var land = {}
-			land.country = e.Country
-			land.GDP = e.Datapoint
+			var land = {};
+			land.country = e.Country;
+			land.GDP = e.Datapoint;
 			
 			// add object for country to the right year
-			clean_data[e.Year].push(land)
-		})
-	})
-	
-	// process ppp and tourists datasets and add to clean_data
-	valuesFun(input[1], "PPP")
-	valuesFun(input[2], "tourists")
-	var i = 0
+			clean_data[e.Year].push(land);
+		});
+	});
 
-	// delete values with not every variable 
-	// iterate over years
+	// process ppp and tourists datasets and add to clean_data
+	valuesFun(input[1], "PPP");
+	valuesFun(input[2], "tourists");
+
+	delCountries([clean_data["2012"]])
 
 	// width & height
-	var w = 600
-	var h = 600
+	var w = 800;
+	var h = 600;
 
 	var svg = d3.select("body")
 				.append("svg")
 				.attr("width", w)
 				.attr("height", h);
+	var tip = d3.tip()
+                    .attr('class', 'd3-tip')
+                    .offset([-10, 0])
+                    .html(function(d) { return "<strong>Country:</strong> <span style='color:darkcyan'>" + d.country + "</span><br/><strong>GDP:</strong> <span style='color:darkcyan'>" + d.GDP + "</span></span><br/><strong>PPP:</strong> <span style='color:darkcyan'>" + d.PPP + "</span>"; });
 
-	// scales
-	var padding = 40
+	// scales and axis
+	var padding = 40;
 	var xScale = d3.scaleLinear()
 					.domain([0, d3.max(clean_data["2012"], function(d) {return d.PPP; })])
-					.rangeRound([padding, w - padding*2])
+					.rangeRound([padding*1.5, w - padding]);
     var yScale = d3.scaleLinear()
 					.domain([d3.min(clean_data["2012"], function(d){ return d.GDP; }),  d3.max(clean_data["2012"], function(d) { return d.GDP; })])
-					.rangeRound([h - padding*2, padding])
-	// var rScale 
+					.rangeRound([h - padding, padding]);
+	var colorScale = d3.scaleSequential(d3.interpolateCool)
+					.domain([0, d3.max(clean_data["2012"], function(d){ return d.tourists; })]);
+
+	var xAxis = d3.axisBottom(xScale)
+					.ticks(8);
+	var yAxis = d3.axisLeft(yScale)
+					.ticks(8)
+					.tickFormat(d3.format(".0s"));
+	var colorLegend = d3.legendColor(colorScale);
+
+	svg.call(tip);
 	
+	// create axis and labels
+	svg.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate(" + 0 + "," + (h - padding) + ")")
+		.call(xAxis);
+    svg.append("text")
+		.attr("transform", "translate(" + w/2 + "," + (h - padding/2) + ")")
+		.style("text-anchor", "start")
+		.style("alignment-baseline", "hanging")
+		.text("PPP");
+
+	svg.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate(" + padding*1.5 + "," + 0 + ")")
+		.call(yAxis);
+	svg.append("text")
+		.attr("transform", "rotate(-90)")
+		.attr("x", - (h / 2))
+		.attr("y", padding/1.5)
+		.attr("text-anchor", "middle")
+		.text("GDP");
+	svg.append("g")
+		.attr("class", "legend")
+		.attr("transform", "translate(" + 2*padding + "," + padding + ")")
+		.call(colorLegend);
+
+	//create actual cirkels
 	svg.selectAll("circle")
 		.data(clean_data["2012"])
 		.enter()
@@ -78,6 +112,9 @@ function plotFun(input) {
 			return yScale(d.GDP);			
 		})
 		.attr("r", 5)
+		.attr("fill", function(d){
+			return colorScale(d.tourists);
+		});
 
 }
 
@@ -86,19 +123,31 @@ function valuesFun(i, name) {
 	Object.values(i).forEach(function(d){
 		// INPUT DATA: iterate over years in input data
 		d.forEach(function(e) {
-			
 			// CLEAN DATA: if year in cleandata -> select the right year & country
-			if ( typeof(clean_data[e.Time]) !== "undefined") {
+			if (typeof(clean_data[e.Time]) !== "undefined") {
 				clean_data[e.Time].forEach(function(f){
 					if (f.country === e.Country) {
 						// add the datapoint to clean_data variable
-						f[name] = e.Datapoint
-					}
-				})
+						f[name] = e.Datapoint;
+					};
+				});
+			};
+		});
+	});
+};
+
+function delCountries(data) {
+	Object.values(data).forEach(function(d){
+		d.forEach(function(e){
+			if (typeof(e.PPP) == "undefined") {
+				d.splice(d.indexOf(e), 1);
 			}
-		})
-	})
-}
+			else if (typeof(e.tourists) == "undefined"){
+				d.splice(d.indexOf(e), 1);
+			};
+		});
+	});
+};
 
 
 
